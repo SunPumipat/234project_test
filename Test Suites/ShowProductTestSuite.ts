202066a092388235af12b1e6fcf0f7a5eb4c69a8
sun_pumipat <pumipat.tip@gmail.com>
2018-05-10 22:03:20 +0700

    create blank testsuite: ShowProductTestSuite

---
+++ Test Suites/ShowProductTestSuite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ShowProductTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>095f7200-e1c7-4c7d-9730-921126ae45a9</testSuiteGuid>
+</TestSuiteEntity>
