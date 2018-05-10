fc1aee7a16c3d11bb3e9f40ab66ceb3bc190fe76
sun_pumipat <pumipat.tip@gmail.com>
2018-05-10 14:22:01 +0700

    create test suite: OpenBrowserTestSuite

---
+++ Test Suites/OpenBrowserTestSuite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>OpenBrowserTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5472d89d-ba8a-4001-9c2a-3160a12dc14c</testSuiteGuid>
+</TestSuiteEntity>
