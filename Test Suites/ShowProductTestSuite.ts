0599cdcb6f56a3ebc4815a762ce23053b69d7614
sun_pumipat <pumipat.tip@gmail.com>
2018-05-10 22:25:41 +0700

    create blank testcase: TotalTransactionTest

--- Test Suites/ShowProductTestSuite.ts
+++ Test Suites/ShowProductTestSuite.ts
@@ -4,10 +4,38 @@
    <name>ShowProductTestSuite</name>
    <tag></tag>
    <isRerun>false</isRerun>
+   <lastRun>2018-05-10T22:23:58</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>095f7200-e1c7-4c7d-9730-921126ae45a9</testSuiteGuid>
+   <testCaseLink>
+      <guid>e66dd17b-7aa5-41cf-943a-9be595aa19bd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ShowProductTest</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>ae1bfe3d-ae1f-42b8-80e7-ead1120ccb09</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/ShowProductTestData</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>ae1bfe3d-ae1f-42b8-80e7-ead1120ccb09</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>5790e907-201a-4f04-8839-82893f471acd</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>ae1bfe3d-ae1f-42b8-80e7-ead1120ccb09</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>edb99c3d-60cd-4d7f-844f-ac0344fdb95a</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
