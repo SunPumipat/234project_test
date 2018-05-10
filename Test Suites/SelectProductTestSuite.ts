64876c7d823ae150842dcac4301e542b2b63116c
sun_pumipat <pumipat.tip@gmail.com>
2018-05-11 00:37:49 +0700

    create testsuite/testdata

---
+++ Test Suites/SelectProductTestSuite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SelectProductTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>646e0f51-9e5a-4f1b-b5a5-2973592a3b2d</testSuiteGuid>
+</TestSuiteEntity>
