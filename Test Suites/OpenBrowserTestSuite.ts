e87a8d8027a79a3545ca027ce7fb3c64e0fd644b
sun_pumipat <pumipat.tip@gmail.com>
2018-05-10 14:27:45 +0700

    add information OpenBrowserTestSuite

--- Test Suites/OpenBrowserTestSuite.ts
+++ Test Suites/OpenBrowserTestSuite.ts
@@ -4,10 +4,17 @@
    <name>OpenBrowserTestSuite</name>
    <tag></tag>
    <isRerun>false</isRerun>
+   <lastRun>2018-05-10T14:26:45</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>5472d89d-ba8a-4001-9c2a-3160a12dc14c</testSuiteGuid>
+   <testCaseLink>
+      <guid>4c15bae2-93bf-4089-8589-4d5313db4ae7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/OpenBrowserTest</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
