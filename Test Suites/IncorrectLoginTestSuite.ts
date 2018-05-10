14ed0b7783e66035f588db65d0c7e5f3fe85a0fd
sun_pumipat <pumipat.tip@gmail.com>
2018-05-10 18:45:24 +0700

    create blank testsuite: IncorrectLoginTestSuite

---
+++ Test Suites/IncorrectLoginTestSuite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>IncorrectLoginTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3b64805e-47c7-431a-80ba-ed391eda0a9a</testSuiteGuid>
+</TestSuiteEntity>
