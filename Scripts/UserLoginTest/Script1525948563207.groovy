b217d76e627b6544dd101e8886dfe9818a2fe712
sun_pumipat <pumipat.tip@gmail.com>
2018-05-10 17:50:06 +0700

    create script: UserLoginTest

--- Scripts/UserLoginTest/Script1525948563207.groovy
+++ Scripts/UserLoginTest/Script1525948563207.groovy
@@ -18,4 +18,19 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://52.14.107.181:8085/')
+
+WebUI.setText(findTestObject('UserLoginTest/Page_ProjectBackend/input_username'), username)
+
+WebUI.setText(findTestObject('UserLoginTest/Page_ProjectBackend/input_password'), password)
+
+WebUI.click(findTestObject('UserLoginTest/Page_ProjectBackend/button_Login'))
+
+WebUI.click(findTestObject('UserLoginTest/Page_ProjectBackend/a_Carts'))
+
+WebUI.closeBrowser()
 
