38e14ec6da8efb0dde914deb20806debbc356099
sun_pumipat <pumipat.tip@gmail.com>
2018-05-10 21:07:11 +0700

    create blank testcase: PassRequiredTest

--- Test Suites/UserRequiredTestSuite.ts
+++ Test Suites/UserRequiredTestSuite.ts
@@ -4,10 +4,32 @@
    <name>UserRequiredTestSuite</name>
    <tag></tag>
    <isRerun>false</isRerun>
+   <lastRun>2018-05-10T21:03:08</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>b478c83b-732d-4b33-9a70-5bbf0b5af990</testSuiteGuid>
+   <testCaseLink>
+      <guid>65eeebee-cbf5-40b3-b461-579e88b731b9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserRequiredTest</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>fc0200f8-7099-4c3d-b5c8-bf2eeb5bf7ba</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/UserRequiredTestData</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>fc0200f8-7099-4c3d-b5c8-bf2eeb5bf7ba</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>41a1b662-2151-4fd0-a9a1-9bc2475cd9c5</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
