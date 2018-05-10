7dba6d4ccc9d2ed48b24ebc8958b8fb3ad839a5e
sun_pumipat <pumipat.tip@gmail.com>
2018-05-10 19:59:06 +0700

    create blank testsuite: UserRequiredTest

---
+++ Test Suites/UserRequiredTestSuite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UserRequiredTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b478c83b-732d-4b33-9a70-5bbf0b5af990</testSuiteGuid>
+</TestSuiteEntity>
