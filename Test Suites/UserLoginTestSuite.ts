a74760650570287ea07e46bb39c5937a19d1aafc
sun_pumipat <pumipat.tip@gmail.com>
2018-05-10 17:37:03 +0700

    create blank testsuite: UserLoginTestSuite

---
+++ Test Suites/UserLoginTestSuite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UserLoginTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7fa56d8c-7869-46f1-8975-c68ba974f3fb</testSuiteGuid>
+</TestSuiteEntity>
