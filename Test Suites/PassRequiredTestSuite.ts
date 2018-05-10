b99fbff3f481c84d43cbe06375e01edaa8f23a93
sun_pumipat <pumipat.tip@gmail.com>
2018-05-10 21:07:43 +0700

    create blank testsuite: PassRequiredTest

---
+++ Test Suites/PassRequiredTestSuite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>PassRequiredTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>660e106b-941b-4089-84f0-6632389ea973</testSuiteGuid>
+</TestSuiteEntity>
